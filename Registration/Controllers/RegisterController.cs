e116b6587f74119dc8a5e244120bc9301e371c69
kamoklekchyan@gmail.com <kamoklekchyan@gmail.com>
2019-08-17 17:14:18 +0400

    Добавьте файлы проекта.

---
+++ Registration/Controllers/RegisterController.cs
@@ -0,0 +1,128 @@
+﻿using Registration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Hosting;
+using System.Net.Mail;
+using System.Web.Mvc;
+
+namespace Registration.Controllers
+{
+    public class RegisterController : Controller
+    {
+        MVCTutorialsEntities db = new MVCTutorialsEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult SaveData(SiteUser model)
+        {
+            model.IsValid = false;
+            db.SiteUser.Add(model);
+            db.SaveChanges();
+            BuildEmailTemplate(model.ID);
+            return Json("Registration Succesfull", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Confirm(int regId)
+        {
+            ViewBag.regID = regId;
+            return View();
+        }
+        public JsonResult RegisterConfirm(int regId)
+        {
+            SiteUser Data = db.SiteUser.Where(x => x.ID == regId).FirstOrDefault();
+            Data.IsValid = true;
+            db.SaveChanges();
+            var msg = "Your Email Verified!";
+            return Json(msg, JsonRequestBehavior.AllowGet);
+        }
+        public void BuildEmailTemplate(int regID)
+        {
+            string body = System.IO.File.ReadAllText(HostingEnvironment.MapPath("~/EmailTemplate/") + "Text" + ".cshtml");
+            var regInfo = db.SiteUser.Where(x => x.ID == regID).FirstOrDefault();
+            var url = "https://localhost:44324/" + "Register/Confirm?regId=" + regID;
+            body = body.Replace("@ViewBag.ConfirmationLink", url);
+            body = body.ToString();
+            BuildEmailTemplate("Your Account Is Succesfully Created", body, regInfo.Email);
+        }
+
+        public static void BuildEmailTemplate(string subjectText, string bodyText, string sendTo)
+        {
+            string from, to, bcc, cc, subject, body;
+            from = "kamoklekchyan@gmail.com";
+            to = sendTo.Trim();
+            bcc = "";
+            cc = "";
+            subject = subjectText;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(bodyText);
+            body = sb.ToString();
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(from);
+            mail.To.Add(new MailAddress(to));
+            if (!string.IsNullOrEmpty(bcc))
+            {
+                mail.Bcc.Add(new MailAddress(bcc));
+            }
+            if (!string.IsNullOrEmpty(cc))
+            {
+                mail.CC.Add(new MailAddress(cc));
+            }
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+            SendEmail(mail);
+        }
+
+        public static void SendEmail(MailMessage mail)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new System.Net.NetworkCredential("kamoklekchyan@gmail.com", "Kam222115");
+            try
+            {
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public JsonResult CheckValidUser(SiteUser model)
+        {
+            string result = "Fail";
+            var DataItem = db.SiteUser.Where(x => x.Email.Equals(model.Email) && x.Password.Equals(model.Password)).SingleOrDefault();
+            if (DataItem != null)
+            {
+                Session["UserID"] = DataItem.ID.ToString();
+                Session["UserName"] = DataItem.UserName.ToString();
+                result = "Success";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult AfterLogin()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+    }
+}
